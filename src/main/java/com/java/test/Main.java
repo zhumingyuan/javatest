d52b19cd0b3d25541579a22b2ef9b6ff93e4c433
zhumingyuan <461624128@qq.com>
2017-12-21 22:24:54 +0800

    java test main and jumit

---
+++ src/main/java/com/java/test/Main.java
@@ -0,0 +1,8 @@
+package com.java.test;
+
+public class Main {
+
+    public static void main(String[]args){
+        System.out.println("hello world ! ");
+    }
+}
---
+++ src/test/java/com/java/test/MainTest.java
@@ -0,0 +1,12 @@
+package com.java.test;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void mainTest(){
+        assert true;
+    }
+
+}
